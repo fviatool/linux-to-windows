518e1dec90994fe16ac63a63b3a1de5585cb1e54
FVIA <130783224+fviatool@users.noreply.github.com>
2024-03-25 06:33:38 +0700

    Update ao.sh

--- ao.sh
+++ ao.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 #
-#Vars
+# Vars
 mounted=0
-GREEN='\033[1;32m';GREEN_D='\033[0;32m';RED='\033[0;31m';YELLOW='\033[0;33m';BLUE='\033[0;34m';NC='\033[0m'
+GREEN='\033[1;32m';RED='\033[0;31m';YELLOW='\033[0;33m';BLUE='\033[0;34m';NC='\033[0m'
 
-# Xóa cài đặt Windows trước đó bằng kịch bản
+
+# Xóa cài đặt Windows và Ubuntu trước đó
 umount -l /mnt /media/script /media/sw
 rm -rf /mediabots /floppy /virtio /media/* /tmp/*
-rm -f /sw.iso /disk.img
+rm -f /sw.iso /disk.img 
 
-# Cài đặt gói Ubuntu cần thiết
+# Cài đặt gói cần thiết cho Ubuntu
 dist=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
 if [ $dist = "CentOS" ]; then
     printf "Y\n" | yum install sudo -y
     sudo yum install wget vim curl genisoimage -y
-    # Tải xuống QEMU-KVM Portable
     echo "Đang tải loading..."
     sudo yum update -y
     sudo yum install -y qemu-kvm
 elif [ $dist = "Ubuntu" -o $dist = "Debian" ]; then
     printf "Y\n" | apt-get install sudo -y
     sudo apt-get install vim curl genisoimage -y
-    # Tải xuống QEMU-KVM Portable
     echo "Đang tải...."
     sudo apt-get update
     sudo apt-get install -y qemu-kvm
@@ -38,54 +37,38 @@ fi
 # Kiểm tra trạng thái của libvirt
 libvirt_status=$(systemctl is-active libvirtd)
 
-# Kiểm tra xem libvirt đã được kích hoạt chưa
+# Kích hoạt libvirt nếu chưa được kích hoạt
 if [ $libvirt_status = "active" ]; then
     echo -e "${GREEN}Libvirt đã được kích hoạt.${NC}"
 else
-    # Kích hoạt libvirt nếu chưa được kích hoạt
     sudo systemctl start libvirtd
     echo -e "${GREEN}Đã kích hoạt Libvirt.${NC}"
 fi
 
-# Kiểm tra xem có user hiện tại trong nhóm libvirt không
+# Thêm user hiện tại vào nhóm libvirt nếu chưa có
 current_user=$(whoami)
 if id "$current_user" | grep -q libvirt; then
     echo -e "${GREEN}Người dùng hiện tại đã được thêm vào nhóm libvirt.${NC}"
 else
-    # Thêm user hiện tại vào nhóm libvirt nếu chưa có
     sudo usermod -aG libvirt $current_user
     echo -e "${GREEN}Đã thêm người dùng hiện tại vào nhóm libvirt.${NC}"
 fi
 
-# Menu chọn cài đặt Windows
-echo -e "${GREEN}Menu cài đặt Windows:${NC}"
-echo "1. Cài đặt Windows 10"
-echo "2. Cài đặt Windows Server 2012"
-read -t 15 -p "Chọn phiên bản Windows bạn muốn cài đặt (nhập số): " choice
+# Hiển thị thông tin người dùng
+echo -e "\nThông tin người dùng hiện tại:"
+current_user=$(whoami)
+echo "Tên người dùng: $current_user"
+echo "Danh sách người dùng và mật khẩu:"
+cat /etc/passwd | awk -F: '{print "Người dùng: " $1 ", Mật khẩu: " $2}'
+echo "Địa chỉ IP của máy:"
+ip_addr=$(hostname -I)
+echo "$ip_addr"
 
-case $choice in
-    1)
-        # Cài đặt Windows 10
-        echo "Bạn đã chọn cài đặt Windows 10"
-        # Thêm mã để tải tài nguyên cần thiết cho Windows 10 ở đây
-        sudo wget -P /mediabots https://download.microsoft.com/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO
-        ;;
-    2)
-        # Cài đặt Windows Server 2012
-        echo "Bạn đã chọn cài đặt Windows Server 2012"
-        # Thêm mã để tải tài nguyên cần thiết cho Windows Server 2012 ở đây
-        sudo wget -P /mediabots https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO
-        ;;
-    *)
-        # Cài đặt Windows Server 2012 mặc định sau 15 giây
-        echo "Không có lựa chọn được chọn. Tự động cài đặt Windows Server 2012 sau 15 giây..."
-        sleep 15
-        # Thêm mã để tải tài nguyên cần thiết cho Windows Server 2012 ở đây
-        sudo wget -P /mediabots https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO
-        ;;
-esac
-
-# Tạo tập lệnh PowerShell để kích hoạt kết nối máy tính từ xa
+# Cài đặt Windows Server 2012
+echo "Bạn đã chọn cài đặt Windows Server 2012"
+echo "Tải tài nguyên cần thiết cho Windows Server 2012..."
+sudo wget -P /mediabots https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO
+echo "Tạo tập lệnh PowerShell để kích hoạt kết nối máy tính từ xa..."
 sudo touch /floppy/EnableRDP.ps1
 sudo cat <<EOF | sudo tee -a /floppy/EnableRDP.ps1
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name "fDenyTSConnections" -Value 0
@@ -93,34 +76,55 @@ Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStat
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 EOF
 
-# Kích hoạt RAM ảo
+sudo wget -P /floppy https://ftp.mozilla.org/pub/firefox/releases/64.0/win32/en-US/Firefox%20Setup%2064.0.exe
+sudo mv /floppy/'Firefox Setup 64.0.exe' /floppy/Firefox.exe
+sudo wget -P /floppy https://downloadmirror.intel.com/23073/eng/PROWinx64.exe
+
+# Tạo tập lệnh PowerShell để kích hoạt kết nối máy tính từ xa
+sudo tee -a /floppy/EnableRDP.ps1 > /dev/null <<'EOF'
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name "fDenyTSConnections" -Value 0
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\' -Name "UserAuthentication" -Value 1
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+EOF
+
+# Tải về trình điều khiển Virtio
+sudo wget -P /virtio https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+
+# Tạo file ISO chứa các tài nguyên và trình điều khiển Windows
+sudo mkisofs -o /sw.iso /floppy
+
+# Bật KSM
+sudo echo 1 > /sys/kernel/mm/ksm/run
 sudo echo 1 > /proc/sys/vm/overcommit_memory
+echo "Mở cổng cho các dịch vụ..."
 sudo ufw allow 3398/tcp
 sudo ufw allow 443/tcp
 sudo ufw allow 80/tcp
 sudo ufw --force enable
-
-# Giải phóng bộ nhớsync
+echo "Giải phóng bộ nhớ..."
+sync
 sudo echo 3 > /proc/sys/vm/drop_caches
 
 # Thu thập thông tin về hệ thống
 idx=0
-hddlist=””
+hddlist=""
 while read line; do
-((idx++))
-if [ $idx = 1 ]; then
-hddlist=”$line”
-else
-hddlist=”$hddlist,$line”
-fi
-done < <(lsblk -lp | grep -o ‘^/dev/sd[^ ]*’)
-echo -e “${GREEN}Các ổ cứng có sẵn trong hệ thống của bạn :${NC} ${YELLOW}$hddlist${NC}”
-echo -e “${GREEN}Tổng RAM Hệ thống :${NC} ${YELLOW}$(grep MemTotal /proc/meminfo | awk ‘{print $2/1024}’)${NC}”
+    ((idx++))
+    if [ $idx = 1 ]; then
+        hddlist="$line"
+    else
+        hddlist="$hddlist,$line"
+    fi
+done < <(lsblk -lp | grep -o '^/dev/sd[^ ]*')
+echo -e "${GREEN}Các ổ cứng có sẵn trong hệ thống của bạn :${NC} ${YELLOW}$hddlist${NC}"echo -e “${GREEN}Tổng RAM Hệ thống :${NC} ${YELLOW}$(grep MemTotal /proc/meminfo | awk ‘{print $2/1024}’)${NC}”
 
-Script kết thúc
+Kết thúc
 
 echo -e “\n${BLUE}Mọi tài nguyên cần thiết đã được tải thành công!${NC}”
 sleep 2
-echo -e “\n${YELLOW}Đang khởi động máy ảo QEMU..${NC}\n”
+echo -e “\n${YELLOW}Khởi động máy ảo QEMU..${NC}\n”
 sleep 3
+
+Khởi động máy ảo Windows Server 2012
+
 sudo qemu-system-x86_64 -m 3072 -smp cores=4 -enable-kvm -cpu host -cdrom /mediabots/WS2012R2.ISO -drive file=disk.img,if=virtio -boot order=d -net nic -net user -usb -usbdevice mouse -usbdevice keyboard -vga std
