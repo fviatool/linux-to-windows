180651e696818fca8bd7078eeb82fff3b4a57ded
FVIA <130783224+fviatool@users.noreply.github.com>
2024-03-25 06:20:30 +0700

    Update ao.sh

--- ao.sh
+++ ao.sh
@@ -12,23 +12,15 @@ else
 #umount -l /mnt /media/script /media/sw
 #rm -rf /mediabots /floppy /virtio /media/* /tmp/*
 #rm -f /sw.iso /disk.img 
-
-# Cài đặt gói Ubuntu cần thiết
-dist=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
-if [ $dist = "CentOS" ]; then
-    printf "Y\n" | yum install sudo -y
-    sudo yum install wget vim curl genisoimage -y
-    # Tải xuống QEMU-KVM Portable
-    echo "Đang Tải loading..."
-    sudo yum update -y
-    sudo yum install -y qemu-kvm
-elif [ $dist = "Ubuntu" -o $dist = "Debian" ]; then
-    printf "Y\n" | apt-get install sudo -y
-    sudo apt-get install vim curl genisoimage -y
-    # Tải xuống QEMU-KVM Portable
-    echo "Đang Tải...."
-    sudo apt-get update
-    sudo apt-get install -y qemu-kvm
+# installing required Ubuntu packages
+#dist=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
+#if [ $dist = "CentOS" ] ; then
+	printf "Y\n" | yum install sudo -y
+	sudo yum install wget vim curl genisoimage -y
+	# Downloading Portable QEMU-KVM
+	echo "Downloading QEMU"
+	sudo yum update -y
+	sudo yum install -y qemu-kvm
 fi
 sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 
